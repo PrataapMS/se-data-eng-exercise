0399df75e86e6cb3cda6a0736e5a0f7d679ee8e0
Prataap.MS <prataap.ms@network.global>
2024-09-11 16:43:08 +0530

    Updated funtion

--- functions/main.py
+++ functions/main.py
@@ -5,7 +5,7 @@ import logging
 
 def load_csv_to_bigquery(file_name, bucket_name):
     if not file_name.endswith('.csv'):
-        logging.info(f"Invalid file: {file_name}, skipped from processing.")
+        print(f"Invalid file: {file_name}, skipped from processing.")
         return
 
     # Extract timestamp from file name
@@ -13,27 +13,27 @@ def load_csv_to_bigquery(file_name, bucket_name):
     try:
         timestamp = datetime.datetime.strptime(timestamp_str, "%Y%m%d%H_%M_%S")
     except ValueError:
-        logging.info(f"Invalid timestamp in file name: {file_name}. Skipping.")
+        print(f"Invalid timestamp in file name: {file_name}. Skipping.")
         return
 
     # Initialize clients
     storage_client = storage.Client()
     bigquery_client = bigquery.Client()
 
-    logging.info(f"Loading data from {file_name}")
+    print(f"Loading data from {file_name}")
 
     # Download the CSV file from the bucket
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(file_name)
 
-    logging.info(f"Loaded data from {file_name}")
+    print(f"Loaded data from {file_name}")
 
     # Define BigQuery dataset and table
     dataset_id = 'movies_data'
     table_id = 'movies_raw'
     table_ref = bigquery_client.dataset(dataset_id).table(table_id)
 
-    logging.info(f"Loading {file_name} to BigQuery.")
+    print(f"Loading {file_name} to BigQuery.")
 
     # Load data into BigQuery
     job_config = bigquery.LoadJobConfig(
@@ -48,13 +48,13 @@ def load_csv_to_bigquery(file_name, bucket_name):
     with blob.open("r") as source_file:
         job = bigquery_client.load_table_from_file(source_file, table_ref, job_config=job_config)
 
-    logging.info("Job loaded.")
+    print("Job loaded.")
 
     job.result()  # Wait for the job to complete.
 
-    logging.info("Job finished.")
+    print("Job finished.")
 
-    logging.info(f"Loaded {job.output_rows} rows into {dataset_id}:{table_id}.")
+    print(f"Loaded {job.output_rows} rows into {dataset_id}:{table_id}.")
 
 def main(event, context):
 
